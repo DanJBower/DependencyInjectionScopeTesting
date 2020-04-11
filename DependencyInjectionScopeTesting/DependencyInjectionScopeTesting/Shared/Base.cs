9f63b25365c6f5033c55f4fcb2e7babba7064945
DanJBower <bowerdanielj@gmail.com>
2020-04-11 13:25:44 +0100

    Initial Commit

---
+++ DependencyInjectionScopeTesting/DependencyInjectionScopeTesting/Shared/Base.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+
+namespace DependencyInjectionScopeTesting.Shared
+{
+    public class Base<T> : ComponentBase, IDisposable
+    {
+        [Inject]
+        public T Service { get; set; }
+
+        public Base()
+        {
+            Debug.WriteLine($"{GetType()} was created.");
+        }
+
+        private bool _disposed;
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Service = default;
+                Debug.WriteLine($"{GetType()} was disposed.");
+            }
+
+            _disposed = true;
+        }
+
+        ~Base()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ DependencyInjectionScopeTesting/DependencyInjectionScopeTesting/Shared/Services.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+
+namespace DependencyInjectionScopeTesting.Shared
+{
+    public abstract class Services : IDisposable
+    {
+        protected Services()
+        {
+            Debug.WriteLine($"{GetType()} was created.");
+        }
+
+        private bool _disposed;
+
+        protected Action ExtraDispose;
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                ExtraDispose?.Invoke();
+                Debug.WriteLine($"{GetType()} was disposed.");
+            }
+
+            _disposed = true;
+        }
+
+        ~Services()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public int Counter { get; set; } = 3;
+    }
+
+    public class ServiceOne : Services
+    {
+    }
+
+    public class ServiceTwo : Services
+    {
+        public ServiceTwo()
+        {
+            Timer timer = new Timer(1000);
+            timer.Elapsed += TimerElapsed;
+            timer.Start();
+
+            ExtraDispose = () => { timer.Stop(); };
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Counter++;
+        }
+    }
+}
